78d9ec6f66665d548d02138bfe15b08e59abed39
Michael-Fares <mjfares@gmail.com>
2021-07-05 12:10:41 -0500

    queries completed

--- RepoQueries.sql
+++ RepoQueries.sql
@@ -1,7 +1,11 @@
-# sample data from individual tables 
+# list data from individual tables 
+
 SELECT * FROM users;
 SELECT * FROM usersAddress;
 SELECT * FROM usersContact;
+
+
+
 
 # 1. Get a sum of all the user_ids from the `usersAddress` table grouped by state. 
 
@@ -22,3 +26,23 @@ state;
  FROM usersContact
  GROUP BY
 SUBSTR(usersContact.phone1, 1, 3);
+
+# Find the MIN first_name, the county, and a count of all users in that county for counties with more than 10 users. There will be four results. List the last one. 
+# Hint: MIN, COUNT, JOIN, GROUP BY, HAVING
+
+  SELECT
+    county,
+    count(user_id) AS tally,
+    MIN(first_name)
+  FROM
+    usersAddress
+  JOIN users
+  WHERE users.id = usersAddress.user_id
+  GROUP BY county
+  HAVING tally > 10;
+
+
+
+
+
+
